bd79c6196a8f7816b184ae3a65ff9db11a77f481
Andrewlittle97 <andrewlittle@mail.weber.edu>
2017-04-04 16:30:37 +0000

    initial

---
+++ hw9_task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hw9_task2.c
+ *       	Usage:  ./hw9_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/04/2017 04:30:03 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Andrew Little (), andrewlittle@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
