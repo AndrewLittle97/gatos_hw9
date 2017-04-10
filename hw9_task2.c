3dfeed3d760b4dcdc7433b53f94c3d4a9cfbbf21
Andrewlittle97 <andrewlittle@mail.weber.edu>
2017-04-10 03:57:32 +0000

    no idea

--- hw9_task2.c
+++ hw9_task2.c
@@ -13,17 +13,39 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#include "zone1.txt"
 
 /* Function Prototypes */
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	count = 0;
+	int sumx, sumy, sumxy, sumx2 = 0;
+	fopen zone1.txt
+		if
+	
 
+	printf("Range of altitudes in km:\n");
+
+	printf("Linear model:\n
+			ozone-mix-ratio = %d altitude = %d" /*zone 1 stuff */);
+			
 	return 0;
 }
 
 
 /* Function Defenitions */
+FILE *FileRead(char *zone1.txt);
+{
+	 FILE *inFile;
 
+inFile = fopen("zone1.txt","r"):
+
+if (inFile == NULL)
+{
+	exit(1);
+}
+	return;
+}
 
