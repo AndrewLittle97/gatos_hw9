83947ce665be7c4c6a737dc2446d8479dd963276
Andrewlittle97 <andrewlittle@mail.weber.edu>
2017-04-10 04:13:59 +0000

    super broken

--- hw9_task2.c
+++ hw9_task2.c
@@ -18,17 +18,22 @@
 /* Function Prototypes */
 
 FILE *FileRead(char *zone1.txt);
+float range(float numbers[], int amount);
 /* Main Program */
 int main(int argc, char *argv[])
 {
+/*  I dont understand how to get the values from zone1.txt
+ *  or how to do most of this from scratch */	
 	count = 0;
 	int sumx, sumy, sumxy, sumx2 = 0;
+	float numbers;
+	numbers = *zone1.txt;
 	fopen zone1.txt
 		if
 	
 
 	printf("Range of altitudes in km:\n");
-
+	
 	printf("Linear model:\n
 			ozone-mix-ratio = %d altitude = %d" /*zone 1 stuff */);
 			
@@ -49,4 +54,23 @@ if (inFile == NULL)
 }
 	return;
 }
+int findMax(int *vals, FILE *zone1.txt)
+{
+	int i, max = vals[0];
 
+	for (i = 1; i < *zone1.txt; i++
+}
+float range(float numbers[], int amount)
+{
+	int i;
+	float min, max;
+	min = numbers[0];
+	max = numbers[0];
+	for (i=1; i < amount; i++)
+	{
+		if (numbers[i] < min) min = numbers[i];
+		if (numbers[i] > max) max = numbers[i];
+	}
+
+	}
+}
